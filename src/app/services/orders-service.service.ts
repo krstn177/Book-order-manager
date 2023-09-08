acc0fc94a5a68e0234fd47c6258d2bebb3736314
krstn177 <christiangeshev@gmail.com>
2023-09-08 14:27:35 +0300

    adjusting env variables

--- src/app/services/orders-service.service.ts
+++ src/app/services/orders-service.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Order } from '../models/order'; 
 import { AuthService } from './auth-service.service';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -33,21 +34,21 @@ export class OrdersServiceService {
   getByCompleted() {
     this.headerReSetter();
     const reqOptionsQuery = {...this.requestOptions, params: new HttpParams().set('where', 'isCompleted=true')};
-    return this.http.get<Order[]>('api/orders', reqOptionsQuery);
+    return this.http.get<Order[]>(`${environment.apiUrl}/orders`, reqOptionsQuery);
   }
 
   getByNotCompleted() {
     this.headerReSetter();
     const reqOptionsQuery = {...this.requestOptions, params: new HttpParams().set('where', 'isCompleted=false')};
-    return this.http.get<Order[]>('api/orders', reqOptionsQuery);
+    return this.http.get<Order[]>(`${environment.apiUrl}/orders`, reqOptionsQuery);
   }
 
   getOrders() {
-    return this.http.get<Order[]>('api/orders', this.requestOptions);
+    return this.http.get<Order[]>(`${environment.apiUrl}/orders`, this.requestOptions);
   }
 
   completeOrder(id: string) {
-    return this.http.get<any>(`api/orders/${id}/complete`, this.requestOptions);
+    return this.http.get<any>(`${environment.apiUrl}/orders/${id}/complete`, this.requestOptions);
   }
 
   updateOrder(id: string, firstName: string, lastName: string, email: string, address: string, phoneNumber: string, count: number) {
@@ -61,11 +62,11 @@ export class OrdersServiceService {
       count: count
     }
 
-    return this.http.put<any>(`api/orders/${id}`, JSON.stringify(order), this.requestOptions);
+    return this.http.put<any>(`${environment.apiUrl}/orders/${id}`, JSON.stringify(order), this.requestOptions);
   }
 
   deleteOrder(id: string) {
-    return this.http.delete<any>(`api/orders/${id}`, this.requestOptions);
+    return this.http.delete<any>(`${environment.apiUrl}/orders/${id}`, this.requestOptions);
   }
 
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://easy-red-clownfish-veil.cyclic.cloud'
+};
